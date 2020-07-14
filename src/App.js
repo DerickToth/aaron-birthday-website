1004653023fa9dd3078abcde08c0fd74cb455e63
Melissa Zhang <mzhang19096@gmail.com>
2020-07-14 12:33:33 -0700

    removed unnecessary template stuff

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    var memeSrc = "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png";
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>IT'S AARON DAY!!!</p>
+          <img className="meme" src={memeSrc} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
