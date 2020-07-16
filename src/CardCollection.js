38f0891c6e0235a7370248fd9b7331d8f81c8357
Melissa Zhang <mzhang19096@gmail.com>
2020-07-15 17:34:56 -0700

    better styling

--- src/CardCollection.js
+++ src/CardCollection.js
@@ -8,18 +8,18 @@ export class CardCollection extends React.Component {
 
       this.state = {
         data: [
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"],
-          ["https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png"]
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"],
+          ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/166f59e9-66e4-4215-bf28-f772832d0922/dccjo0j-142d0303-126b-4f82-87ff-ad06768a764f.png", "https://i.pinimg.com/originals/22/27/c0/2227c0567796bde1273dc37874ebcea2.png"]
         ],
       }
     }
